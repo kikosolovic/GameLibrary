d3aac5de3c3cb5f2de8cadcef4d2c5bba6a903a3
Tabak666 <roman.tabacek2005@gmail.com>
2025-11-07 16:47:08 +0100

    Django Setup

---
+++ gamelibrary/main/urls.py
@@ -0,0 +1,10 @@
+# main/urls.py
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),  # main page
+    path('login/', views.login_view, name='login'),
+    path('register/', views.register_view, name='register'),
+    path('game/<int:id>/', views.gamecard, name='gamecard'),  # static for now
+]
---
+++ gamelibrary/main/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    return render(request, 'index.html')
+
+def login_view(request):
+    return render(request, 'login.html')
+
+def register_view(request):
+    return render(request, 'register.html')
+
+def gamecard(request, id=None):
+    return render(request, 'gamecard.html')
