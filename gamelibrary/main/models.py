2f557ecd00c44bf90ca8362cb3c546a8a67ca872
Tabak666 <roman.tabacek2005@gmail.com>
2025-11-09 13:33:50 +0100

    working auth & guest login

--- gamelibrary/main/models.py
+++ gamelibrary/main/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+GENRE_CHOICES =[
+    ('action', 'Action'),
+    ('adventure', 'Adventure'),
+    ('rpg', 'RPG'),
+    ('strategy', 'Strategy'),
+    ('simulation', 'Simulation'),
+    ('sports', 'Sports'),
+    ('racing', 'Racing'),
+    ('horror', 'Horror'),
+    ('indie', 'Indie'),
+]
+
+GENDER_CHOICES = [
+    ('M', 'Male'),
+    ('F', 'Female'),
+]
+
+class Users(models.Model):
+    email = models.EmailField(unique=True)
+    username = models.CharField(max_length=100, unique=True)
+    password = models.CharField(max_length=256)
+    favourite_genre = models.CharField(max_length=100, choices=GENRE_CHOICES)
+    gender = models.CharField(max_length=6, choices=GENDER_CHOICES)
--- gamelibrary/main/urls.py
+++ gamelibrary/main/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('login/', views.login_view, name='login'),
     path('register/', views.register_view, name='register'),
     path('game/<int:id>/', views.gamecard, name='gamecard'),  # static for now
+    path('logout/',views.logout_view, name='logout'),
 ]
